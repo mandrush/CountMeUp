e2dfc1b123455ef81cc9287c6244a2ef977e000c
mandrush <andruszkiewiczm1@gmail.com>
2018-05-19 22:42:52 +0200

    settings layout defined

--- app/src/main/java/com/wpam18l/countmeup/SettingsActivity.kt
+++ app/src/main/java/com/wpam18l/countmeup/SettingsActivity.kt
@@ -2,11 +2,16 @@ package com.wpam18l.countmeup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+    }
+
+    fun saveSettings(view: View) {
+
     }
 }
